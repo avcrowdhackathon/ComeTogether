0cfec002911330d31f19a1dcf51918e45a0a5157
Nikos <n.chatzivasileiadis@gmail.com>
2020-04-04 21:44:31 +0300

    init

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Splash} from './src/components'
+import { applyMiddleware, createStore, compose } from 'redux';
+import logger from 'redux-logger';
+import rootReducers from './reducers';
+import { persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import React from 'react'
 
-AppRegistry.registerComponent(appName, () => App);
+
+//redux initialization
+const store = createStore(
+    rootReducers,
+    applyMiddleware(
+        logger,
+        thunk
+    )
+  );
+  
+const persistor = persistStore(store);
+
+const Main = (props) => (
+    <Provider store={store}>
+        <PersistGate loading={<Splash />} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => Main);
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Splash} from './Splash';
+export {default as InsertUser} from './InsertUser';
+export {default as VerifyUser} from './VerifyUser';
+export {default as Login} from './Login';
+export {default as Logout} from './Logout';
+export {default as LogoTitle} from './LogoTitle';

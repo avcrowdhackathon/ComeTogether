098018a6424ab69cbc6fdcc2ac35faa6aed5177c
Nikos <n.chatzivasileiadis@gmail.com>
2020-04-04 21:42:38 +0300

    first routes

---
+++ src/navigators/TabNavigator.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Image} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {InsertUser, VerifyUser} from '../components';
+
+
+
+
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+    return (
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Insert') {
+            iconName = focused? require('../../images/insert_user_active.png'):require('../../images/inser_user_inactive.png');
+          } 
+          if (route.name === 'Verify') {
+            iconName = focused? require('../../images/verify_user_active.png'):require('../../images/verify_user_inactive.png');
+          } 
+
+          // You can return any component that you like here!
+          return <Image source={iconName} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#FF652F',
+        inactiveTintColor: '#B4CFF2',
+        labelPosition:'below-icon',
+        style: {
+          height:70,
+          backgroundColor: 'black'
+        },
+        tabStyle: {
+          alignItems:'center',
+          justifyContent: 'center',
+        },
+        labelStyle: {
+        fontSize:15
+        }
+      }}
+      backBehavior= 'history'
+      initialRouteName= 'Insert'
+    > 
+      <Tab.Screen name="Insert" component={InsertUser} />
+      <Tab.Screen name="Verify" component={VerifyUser} />
+    </Tab.Navigator>
+    );
+  }

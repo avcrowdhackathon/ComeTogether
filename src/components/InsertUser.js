39b0ac1c7c904c9c03fb5c2d24de36a63288f0a6
Nikos <n.chatzivasileiadis@gmail.com>
2020-04-04 21:43:09 +0300

    init

---
+++ src/components/InsertUser.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    View,
+    Text
+  } from 'react-native';
+
+export default function InsertUser() {
+     return (
+       <View>
+           <Text>Insert User page</Text>
+        </View>
+     );
+   }

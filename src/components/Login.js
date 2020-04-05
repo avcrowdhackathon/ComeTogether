f9d1c1ec26256baa5c6e99df6506c35221df29f6
Nikos <n.chatzivasileiadis@gmail.com>
2020-04-05 08:32:47 +0300

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {
   StyleSheet,
   View,
+  ScrollView,
   Text,
   TextInput,
+  Image,
   TouchableHighlight
 } from 'react-native';
 import { AuthContext } from "../../App";
@@ -12,12 +14,42 @@ import { AuthContext } from "../../App";
 export default function Login ({navigation}) {    
   const { signIn } = React.useContext(AuthContext);
 
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+
   return (
       <View style={styles.container}>
-          <Text>Login</Text>
+        <Image style={styles.logo} source={require('../../images/comeTogetherBlack.png')} resizeMode='contain'/>
+        <Text style={styles.header}> Login</Text>
+        
+        <View style={styles.root}>
+          <View style={styles.rowContainer}>
+            <Text style={styles.text}>Username</Text>
+            <TextInput
+              autoCorrect={false}
+              onChangeText={setUsername}
+              value={username}
+              style={styles.textInput}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.text}>Password</Text>
+            <TextInput
+              autoCorrect={false}
+              onChangeText={setPassword}
+              value={password}
+              style={styles.textInput}
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+
+        <View style={styles.buttonContainer}>
           <TouchableHighlight style={styles.scan} title="Login" onPress= {()=>{signIn()}} >
-                    <Text style={styles.button}>Next</Text>
-            </TouchableHighlight>
+            <Text style={styles.button}>Login</Text>
+          </TouchableHighlight>
+        </View>
+        
       </View>
   );
 }
@@ -27,12 +59,14 @@ const styles = StyleSheet.create({
       flex:1,
       backgroundColor: '#F5FCFF',
       justifyContent:"space-around",
-      paddingHorizontal:20
+      paddingHorizontal:20,
+      overflow: "hidden"
     },
     button:{
       fontSize: 20,
       color:'#FFFFFF',
       fontWeight: 'bold',
+      paddingHorizontal:20,
     },
     scan: {
       justifyContent:'center',
@@ -41,6 +75,42 @@ const styles = StyleSheet.create({
       borderRadius:7,
       backgroundColor: '#FF652F',
       marginBottom:100
-  
     },
+    logo: {
+      flex:1,
+      height: undefined,
+      width: undefined
+    },
+    header: {
+      flex:0.5,
+      textAlign: 'center',
+      fontSize: 25,
+      fontWeight: 'bold'
+    },
+    root: {
+      flex: 1,
+      flexDirection: "column",
+    },
+    rowContainer: {
+      flex: 1, 
+      flexDirection: "row",
+      alignItems: "center"
+    },
+    buttonContainer: {
+      width: '100%',
+      flex: 2, 
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    text: {
+      flex: 1
+    },
+    textInput: {
+      flex: 2,
+      paddingLeft: 6,
+      borderWidth: 2,
+      borderRadius: 8,
+      borderColor: 'grey',
+      backgroundColor: 'rgba(243, 241, 239, 0.8)',
+    }
   });
